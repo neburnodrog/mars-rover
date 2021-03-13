6b2bb6c6e428b7408d6817baf0c0cbfae6f3680a
Ruben Karlsson <neburgordon@gmail.com>
2021-03-13 16:10:50 +0100

    version without nunmbers

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -4,59 +4,42 @@ import styled from "styled-components";
 
 
 /**
- *  CELL COMPONENT
+ *  CELL COMPONENT // each cell is a row in the parent column component
  */
 
-const CellStyle = styled.button`
+const Cell = styled.button`
   background-color: transparent;
   border: 1px solid sandybrown;
   color: palevioletred;
 `;
 
-interface CellProps {
-  coordinates: number[];
-}
-
-const Cell = (props: CellProps) => {
-  return(
-    <CellStyle>{props.coordinates}</CellStyle>
-  );
-}
-
-
 /**
- *  ROW COMPONENT
+ *  COL COMPONENT
  */
-
 const ColStyle = styled.div`
   display: grid;
   grid-template-rows: repeat(10, 1fr);
 `;
 
-interface ColProps {
-  colNumber: number;
-}
+class Column extends Component {
 
-class Column extends Component<ColProps> {
-
-  renderCell(i: number) {
-    const coordinates = [this.props.colNumber, i]
-    return <Cell coordinates={coordinates}></Cell>;
+  renderCell() {
+    return <Cell></Cell>;
   }
 
   render() {
     return (
       <ColStyle>
-        {this.renderCell(0)}
-        {this.renderCell(1)}
-        {this.renderCell(2)}
-        {this.renderCell(3)}
-        {this.renderCell(4)}
-        {this.renderCell(5)}
-        {this.renderCell(6)}
-        {this.renderCell(7)}
-        {this.renderCell(8)}
-        {this.renderCell(9)}
+        {this.renderCell()}
+        {this.renderCell()}
+        {this.renderCell()}
+        {this.renderCell()}
+        {this.renderCell()}
+        {this.renderCell()}
+        {this.renderCell()}
+        {this.renderCell()}
+        {this.renderCell()}
+        {this.renderCell()}
       </ColStyle>
     );
   }
@@ -76,23 +59,23 @@ const Grid = styled.div`
 `;
 
 export class GridComponent extends React.Component {
-  renderRow(i: number) {
-    return <Column colNumber={i}></Column>;
+  renderRow() {
+    return <Column></Column>;
   }
 
   render() {
     return (
       <Grid>
-        {this.renderRow(0)}
-        {this.renderRow(1)}
-        {this.renderRow(2)}
-        {this.renderRow(3)}
-        {this.renderRow(4)}
-        {this.renderRow(5)}
-        {this.renderRow(6)}
-        {this.renderRow(7)}
-        {this.renderRow(8)}
-        {this.renderRow(9)}
+        {this.renderRow()}
+        {this.renderRow()}
+        {this.renderRow()}
+        {this.renderRow()}
+        {this.renderRow()}
+        {this.renderRow()}
+        {this.renderRow()}
+        {this.renderRow()}
+        {this.renderRow()}
+        {this.renderRow()}
       </Grid>
     );
   }
